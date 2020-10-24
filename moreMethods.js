68ced8439b9c35473a9c5f44e5c7a5d556ec7288
Ellie S <asila527@gmail.com>
2020-10-23 21:23:53 -0700

    add files

---
+++ moreMethods.js
@@ -0,0 +1,8 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard' //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+const board = word.slice(5, word.length);
+const facialHair = board.replace('o', 'e');
+
+
